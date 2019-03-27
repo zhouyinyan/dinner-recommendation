c4c359a055cd7151e385562031a34a3863899327
zhouyinyan <gruelac@126.com>
2019-03-27 15:19:45 +0800

    init spring boot

---
+++ src/main/java/com/zyy/app/dinner/DinnerApplication.java
@@ -0,0 +1,13 @@
+package com.zyy.app.dinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinnerApplication.class, args);
+	}
+
+}
