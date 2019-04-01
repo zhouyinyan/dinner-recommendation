75ddf3c3b110b6535cd7312a27817e7113052d32
zhouyinyan <gruelac@126.com>
2019-04-01 11:44:25 +0800

    集成weixin4J框架

--- src/test/java/com/zyy/app/dinner/controller/WeixinControllerTest.java
+++ src/test/java/com/zyy/app/dinner/controller/WeixinControllerTest.java
@@ -1,22 +1,31 @@
 package com.zyy.app.dinner.controller;
 
 import com.zyy.app.dinner.DigestUtils;
+import org.apache.catalina.connector.CoyoteInputStream;
+import org.apache.catalina.connector.InputBuffer;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.DelegatingServletInputStream;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.weixin4j.model.message.output.TextOutputMessage;
+import org.weixin4j.spi.IMessageHandler;
 
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.StringJoiner;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,9 +40,12 @@ public class WeixinControllerTest extends BaseMvcTest{
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private IMessageHandler messageHandler;
+
     @Test
-    public void testHello() throws Exception {
-        logger.info("测试Weixin控制器的echo().....开始");
+    public void testGet() throws Exception {
+        logger.info("测试Weixin控制器的get().....开始");
         String timestamp = "timestamp111";
         String nonce = "saldfaslfjas";
         String echostr = "echostr111";
@@ -49,7 +61,7 @@ public class WeixinControllerTest extends BaseMvcTest{
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
                 .andExpect(content().string(echostr));
-        logger.info("测试Weixin控制器的echo().....通过");
+        logger.info("测试Weixin控制器的get().....通过");
     }
 
     private String sign(String timestamp, String nonce, String token){
@@ -59,6 +71,32 @@ public class WeixinControllerTest extends BaseMvcTest{
         list.forEach(e -> stringJoiner.add(e));
         return DigestUtils.sha1DigestAsHex(stringJoiner.toString().getBytes());
     }
+
+//    @Test
+//    public void testPost() throws Exception {
+//        logger.info("测试Weixin控制器的post().....开始");
+//        String timestamp = "timestamp111";
+//        String nonce = "saldfaslfjas";
+//        String echostr = "echostr111";
+//
+//        given(this.messageHandler.invoke(new DelegatingServletInputStream(new ByteArrayInputStream(new byte[10]))))
+//                .willReturn(new TextOutputMessage("消息已处理").toXML());
+//
+//        MockHttpServletRequestBuilder requestBuilder =
+//                MockMvcRequestBuilders.post("/wx")
+//                        .param("timestamp", timestamp)
+//                        .param("nonce", nonce)
+//                        .param("echostr", echostr)
+//                        .param("signature", sign(timestamp, nonce, WeixinController.token))
+//                        .accept(MediaType.TEXT_XML);
+//
+//        mockMvc.perform(requestBuilder)
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(new TextOutputMessage("消息已处理").toXML()));
+//        logger.info("测试Weixin控制器的post().....通过");
+//    }
+
+
 
 
 }
